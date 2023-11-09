3b72a10569710671bb8f6ef2e54f505a11775229
“karthik.ravikumar1” <karthi@Karthiks-MacBook-Air.local>
2023-11-09 23:34:44 +0530

    installed react & dom

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
-const heading = React.createElement("h1", {}, "Hello World from React");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement("h1", {}, "Hello World from React K");
         const root = ReactDOM.createRoot(document.getElementById("root"));
-        root.render(heading);
+        root.render(heading);   
